e5f9d4095af390c2e537d5a08c57c6c5da264d13
Yize <jaccoqiao@gmail.com>
2017-11-13 03:53:40 -0800

    Replace tabs

--- src/main/java/hipchat/Message.java
+++ src/main/java/hipchat/Message.java
@@ -9,69 +9,69 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Message 
 {
-	private List<String> mentions = new ArrayList<>();
-	private List<String> emoticons = new ArrayList<>();
-	private List<Link> links = new ArrayList<>();
+    private List<String> mentions = new ArrayList<>();
+    private List<String> emoticons = new ArrayList<>();
+    private List<Link> links = new ArrayList<>();
 
-	public List<String> getMentions()
-	{
-		return mentions;
-	}
-	public void setMentions(List<String> mentions)
-	{
-		this.mentions = mentions;
-	}
+    public List<String> getMentions()
+    {
+        return mentions;
+    }
+    public void setMentions(List<String> mentions)
+    {
+        this.mentions = mentions;
+    }
 
-	public List<String> getEmoticons()
-	{
-		return emoticons;
-	}
-	public void setEmoticons(List<String> emoticons)
-	{
-		this.emoticons = emoticons;
-	}
+    public List<String> getEmoticons()
+    {
+        return emoticons;
+    }
+    public void setEmoticons(List<String> emoticons)
+    {
+        this.emoticons = emoticons;
+    }
 
-	public List<Link> getLinks()
-	{
-		return links;
-	}
-	public void setLinks(List<Link> links)
-	{
-		this.links = links;
-	}
+    public List<Link> getLinks()
+    {
+        return links;
+    }
+    public void setLinks(List<Link> links)
+    {
+        this.links = links;
+    }
 
-	@JsonAutoDetect
-	public static class Link
+    @JsonAutoDetect
+    public static class Link
     {
         private String url;
         private String title;
 
-		public Link(String url, String title)
-		{
-			this.url = url;
-			this.title = title;
-		}
+        public Link(String url, String title)
+        {
+            this.url = url;
+            this.title = title;
+        }
 
-		public Link()
-		{
-		}
+        public Link()
+        {
+        }
 
-		public String getUrl()
-		{
-			return url;
-		}
-		public void setUrl(String url)
-		{
-			this.url = url;
-		}
+        public String getUrl()
+        {
+            return url;
+        }
+        public void setUrl(String url)
+        {
+            this.url = url;
+        }
 
-		public String getTitle()
-		{
-			return title;
-		}
-		public void setTitle(String title)
-		{
-			this.title = title;
-		}
-	}
+        public String getTitle()
+        {
+            return title;
+        }
+        public void setTitle(String title)
+        {
+            this.title = title;
+        }
+    }
 }
